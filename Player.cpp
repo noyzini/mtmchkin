e290bcf8089e4d34d97d03653d3fd3d0547c69da
noyzini <102484852+noyzini@users.noreply.github.com>
2022-05-02 20:37:59 +0300

    player noy addition

--- Player.cpp
+++ Player.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+const int MAX_LEVEL=10;
+
 Player::Player(string& name, int maxHP, int force) :
 m_name(name),
 m_level(1),
@@ -21,3 +23,29 @@ void Player::printInfo() {
     printPlayerInfo(this->m_name.c_str(), this->m_level, this->m_force, this->m_HP, this->m_coins);
 }
 
+void Player::levelUp(){
+    if(m_level<MAX_LEVEL)
+    {
+        m_level++;
+    }
+}
+
+void Player::buff(int boost) {
+    this->m_force+=boost;
+}
+
+void Player::damage(int damage) {
+    this->m_HP-=damage;
+    if(this->m_HP<0)
+    {
+        this->m_HP=0;
+    }
+}
+
+void  Player::addCoins(int coins) {
+    this->m_coins+=coins;
+}
+
+int Player::getAttackStrength() {
+    return (this->m_force+ this->m_level);
+}
