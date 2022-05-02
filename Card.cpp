05c24c20736f369cd8e1e7887d4b93dd2fa0b8fc
AviaAvraham1 <avia.avraham@campus.technion.ac.il>
2022-05-02 21:28:51 +0300

    Card.cpp apply encounter

--- Card.cpp
+++ Card.cpp
@@ -12,6 +12,43 @@ Card::Card(CardType type, const CardStats& stats)
     this->m_stats = stats;
 }
 
+void Card::applyEncounter(Player &player) const
+{
+    switch (m_effect)
+    {
+        case CardType::Battle:
+            if (player.getAttackStrength() >= m_stats.force)
+            {
+                player.levelUp();
+                player.addCoins(m_stats.loot);
+            }
+            else
+            {
+                player.damage(m_stats.hpLossOnDefeat);
+            }
+            break;
+        case CardType::Treasure:
+            player.addCoins(m_stats.loot);
+            break;
+        case CardType::Heal:
+        case CardType::Buff:
+            if (player.pay(m_stats.cost))
+            {
+                if (m_effect == CardType::Heal)
+                {
+                    player.heal(m_stats.heal);
+                }
+                else
+                {
+                    player.buff(m_stats.buff);
+                }
+            }
+            break;
+        default:
+            break;
+    }
+}
+
 void Card::printInfo() const {
     switch (m_effect) {
         case CardType::Battle:
