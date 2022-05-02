01340b04a23657a4b99adc4517dd89f6028d0959
noyzini <102484852+noyzini@users.noreply.github.com>
2022-05-02 17:17:53 +0300

    let's start

---
+++ Player.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Noy Zini on 02/05/2022.
+//
+
+#include "Player.h"
+#include <string>
+using namespace std;
+
---
+++ Player.h
@@ -0,0 +1,21 @@
+//
+// Created by Noy Zini on 02/05/2022.
+//
+
+#ifndef MTMCHKIN_PLAYER_H
+#define MTMCHKIN_PLAYER_H
+#include <string>
+using namespace std;
+
+class Player{
+    string m_name;
+    int m_level;
+    int m_force;
+    int m_maxHP;
+    int m_HP;
+    int m_coins;
+
+};
+
+
+#endif //MTMCHKIN_PLAYER_H
